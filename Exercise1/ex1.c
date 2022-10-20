d9eb7c83cd7fb04c4c738f3f8aa662dc5a81e1c4
EdoMacbook <bertoliedoardo99@gmail.com>
2022-10-20 10:23:04 +0200

    New graphs in Exercise1/ to put in the report and new folder for integration algorithms

--- Exercise1/ex1.c
+++ Exercise1/ex1.c
@@ -8,30 +8,38 @@ La somma inversa è più precisa poichè all'inizio sommo numeri molto piccoli c
 aggiungo numeri più grandi, quindi non perdo precisione. Con la somma diretta invece quando arrivo a sommare gli ultimi numeri
 con N grande, gli ultimi (quelli dopo N = 10^-4) non verrano considerati. (Leggi come vengono storate le cifre)
 
+IMPORTANTE: la somma inversa è diversa dalla diretta quando sono all'ordine di grandezza delle
+cifre che riesce a mantenere la mantissa, perciò se sono in single point, la mantissa ha 8 elementi, 
+perciò quando arrivo a ordini di grandezza di 2^-8 i numeri successivi non vengono sommati, invece
+con la somma inversa parto sommando numeri dello stesso ordine di grandezza, quindi sono più preciso
+Quello che mi aspetto quindi è che aumentando la precisione il punto di incontro dei due esponenziali
+cresca verso N più grandi.
+Perciò il punto di inversione è quando finisce la mantissa nella somma diretta è log10(2^(cifremantissa/2))
+è l'ordine di grandezza a cui si ferma
 */
 
 #include <stdlib.h>
 #include <stdio.h>
 
 // Somma della serie 1/n^2 partendo da n=1 -> fino a n = N
-long double directSum(int N)
+double directSum(int N)
 {	
-	long double sum = 0;
+	double sum = 0;
 	for (int i = 1; i < N - 1 ; ++i)
 	{
-		sum += 1.0 /  ( (long double)i*(long double)i ) ; 
+		sum += 1.0 /  ( (double)i*(double)i ) ; 
 	}
 
 	return sum;
 }
 
 // Somma della serie 1/n^2 partendo da n=N -> fino a n = 1
-long double inverseSum(int N)
+double inverseSum(int N)
 {	
-	long double sum = 0;
+	double sum = 0;
 	for (int i = N; i > 0 ; --i)
 	{
-		sum += 1.0 / ( (long double)i*(long double)i ) ; 
+		sum += 1.0 / ( (double)i*(double)i ) ; 
 	}
 
 	return sum;
@@ -39,27 +47,27 @@ long double inverseSum(int N)
 
 int main(int argc, char const *argv[])
 {
-	if(argc < 4)
+	if(argc < 5)
 	{
-		printf("Utilizzo programma: ./ex1.unx [Ni = N iniziale], [Np = passo su cui iterare], [Nf = N finale]\n");
+		printf("Utilizzo programma: ./ex1.unx [Ni = N iniziale], [Np = passo su cui iterare], [Nf = N finale], [\" Nome file\"]\n");
 		exit(EXIT_FAILURE);
 	}
 		int Ni = atoi(argv[1]); //N iniziale e counter nell'iterazione
 		int Np = atoi(argv[2]); //Passo con cui aumento N
 		int Nf = atoi(argv[3]); // N finale da raggiungere
 
-		//printf("[long double, single precision]-Somma diretta da 1 a %d = %f\n", N, directSum(N));
-		//printf("[long double, single precision]-Somma inversa da %d a 1 = %f\n", N, inverseSum(N));
+		//printf("[double, single precision]-Somma diretta da 1 a %d = %f\n", N, directSum(N));
+		//printf("[double, single precision]-Somma inversa da %d a 1 = %f\n", N, inverseSum(N));
 
 		FILE *file;
 
-		file = fopen("data.txt", "w");
+		file = fopen(argv[4], "w");
 		//fprintf(file, "N Diretta Inversa\n");
 
 
 		while(Ni < Nf){
 			
-			fprintf(file, "%d,%.100Lf,%.100Lf\n", Ni, directSum(Ni), inverseSum(Ni));
+			fprintf(file, "%d,%.20lf,%.20lf\n", Ni, directSum(Ni), inverseSum(Ni));
 			Ni+=Np;
 			
 		}
--- Exercise1/plot.py
+++ Exercise1/plot.py
@@ -3,41 +3,49 @@ import numpy as np
 
  
 
-x = np.genfromtxt("data.txt", delimiter=',',dtype=float, usecols=0)
-y = np.genfromtxt("data.txt", delimiter=',',dtype=float, usecols=1) 
+x = np.genfromtxt("doppiaPrecisione.txt", delimiter=',',dtype=float, usecols=0)
+y1 = np.genfromtxt("doppiaPrecisione.txt", delimiter=',',dtype=float, usecols=1) 
+y2 = np.genfromtxt("doppiaPrecisione.txt", delimiter=',', dtype=float, usecols=2)
 
-yver = np.zeros(len(y), dtype=float);
+yver1 = np.zeros(len(y1), dtype=float)
+yver2 = np.zeros(len(y2), dtype=float)
 TICKS = np.zeros(950, dtype=float)
+TICKSX = [5000, 10000,20000,100000, 500000]
 counter = 0.005
 for i in range(len(TICKS)):
 	TICKS[i]+=counter
 	counter+=0.005
-for i in range(len(yver)):
-	yver[i] = np.absolute(y[i] - np.pi*np.pi/6.0)
+for i in range(len(yver1)):
+	yver1[i] = np.absolute(y1[i] - np.pi*np.pi/6.0)
+for i in range(len(yver2)):
+	yver2[i] = np.absolute(y2[i] - np.pi*np.pi/6.0)
 
-print(x)
-print(yver)
+#print(x)
+#print(yver1)
 
 
 
 plt.style.use('_mpl-gallery')
 
-
-fig, ax = plt.subplots()
+fig = plt.figure()
+ax0 = fig.add_subplot(1,1,1)
 plt.tight_layout() #fai in modo che il grafico si centri bene nella figura
 fig.set_size_inches(30/2.54, 30/2.54)
 
 
-ax.scatter(x,yver, s=12, c="purple")
+ax0.scatter(x,yver1, s=12, c="purple")
+ax0.scatter(x,yver2, s=12, c="green")
+ax0.legend(['Somma diretta','Somma inversa'])
 
 
-ax.set_xlabel("N [Integer at which the summation stops] - logscale")
-ax.set_ylabel(r'$ | \Delta N = S(N) - \frac{\pi^2}{6} | $')
-ax.set_title("Exercise 1 - Direct Summation - Quadruple_point_precision(128bit)")
-ax.set_yticks(TICKS)
-#ax.set_xscale('log')
+ax0.set_xlabel("N [Integer at which the summation stops]")
+ax0.set_ylabel(r'$ | \Delta N = S(N) - \frac{\pi^2}{6} | $')
+ax0.set_title("Exercise 1 - Direct Summation - Single_Point_Precision(32bit)")
+ax0.set_yticks(TICKS)
+ax0.set_yscale('log')
+#ax0.set_xticks(TICKSX)
 
-#ax.plot(x, yver)
-ax.set(xlim=(0, np.amax(x)), ylim=(0,np.amax(yver) + np.amax(yver)/4))
+#ax0.plot(x, yver1)
+ax0.set(xlim=(np.amin(x), np.amax(x)), ylim=(0,np.amax(yver1) + np.amax(yver1)/4))
 
 plt.show()
