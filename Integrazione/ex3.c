d50bd2f5a213b43b10cbe231bf0de776f2b8485f
EdoMacbook <bertoliedoardo99@gmail.com>
2022-10-26 11:15:07 +0200

    Inizio esercizi integrazione

---
+++ Integrazione/ex3.c
@@ -0,0 +1,9 @@
+#include <integration.h>
+
+/*Per includere i file usare gcc -I./ che include tutti i file nella cartella corrente*/
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
---
+++ Integrazione/integration.h
@@ -0,0 +1,7 @@
+/* File header contenente tutti i metodi di integrazione da utilizzare per gli esercizi 3-4-5-6-7-8*/
+// Edoardo Bertoli - Lab. Fisica Computazionale - 869024
+
+#include<stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
