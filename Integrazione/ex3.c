28223bd9e2c1aeec7aa8353865e23d819bdf1ef7
EdoMacbook <bertoliedoardo99@gmail.com>
2022-10-26 12:16:13 +0200

    Inizio studio metodi di integrazione - 26/10/2022

--- Integrazione/ex3.c
+++ Integrazione/ex3.c
@@ -1,9 +1,42 @@
 #include <integration.h>
+#include <math.h>
 
 /*Per includere i file usare gcc -I./ che include tutti i file nella cartella corrente*/
+/*
+Cosa fare?
+1. calcolare l'integrale con tutti i metodi diversi, plottare la deviazione |valore vero-valore calcolato|
+ e plottarlo in funzione del numero di punti
+ 2. Calcolare l'integrale con legendre e laguerre, domanda ? laguerre è più efficiente di legendre?*/
+long double func(long double x)
+{
+	return pow(x,7)*exp(-x);
+}
 
 int main(int argc, char const *argv[])
 {
+	if(argc<3)
+	{
+		printf("Utilizzo: ./ex3.unx [N = numero pti integrazione] [Type of integration method (1-4)]\n");
+		printf("Metodi di integrazione:\nTrapezio = 1\nSimpson = 2\nRomberg = 3\nGauss = 4\n");
+		printf("exiting...\n");
+		exit(EXIT_FAILURE);
+	}
+	int N = atoi(argv[1]); //Numero di punti su cui dividere l'intervallo e integrare
+	printf("Numero di punti scelti: %d\n",N);
+	int method = atoi(argv[2]);
+	if(method == 1)
+	{
+		printf("Metodo del trapezio, risultato: %Lf\n",trapezio(N,0,5,func));
+	}
+	else if(method == 2)
+	{
+		printf("Metodo di Simpson, risultato: %Lf\n",simpson(N,0,5,func));
+	}
+	else
+	{
+		printf("NOT IMPLEMENTED YET, exiting...\n");
+		exit(EXIT_FAILURE);
+	}
 	
 	return 0;
 }
--- Integrazione/integration.h
+++ Integrazione/integration.h
@@ -1,7 +1,55 @@
 /* File header contenente tutti i metodi di integrazione da utilizzare per gli esercizi 3-4-5-6-7-8*/
 // Edoardo Bertoli - Lab. Fisica Computazionale - 869024
 
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include<math.h>
+#include <math.h>
 
+/*Regola del trapezio: si basa sull'espansione di taylor in uno dei N punti scelti, integrali solo su
+intervallo finito */
+// N = numero di punti su cui eseguire l'integrazione, a e b sono gli estremi di integraziobe
+//Punti equidistanziati h = (b-a) / N, func è un puntatore alla funzione da usare
+//accedere a funzione passata come parametro (*func)(input)
+//INTEGRAZIONE DEL TRAPEZIO
+long double trapezio(int N, long double a, long double b, long double (*func)(long double))
+{
+
+	long double h = (b-a) / (long double)(N-1) ;
+	long double result = 0.0;
+	result += func(a)*0.5; //aggiungo 1/2 f(a)
+	long double point = a+h;
+	while(point < b){
+	
+		result+=func(point);
+		point+=h;
+		//printf("%Lf\n",point);
+	}
+	result+= func(b)*0.5; //aggiungo 1/2 f(b)
+	result*=h;
+
+	return result;
+}
+
+//INTEGRAZIONE DI SIMPSON
+long double simpson(int N, long double a, long double b, long double (*func)(long double))
+{
+	long double h = (b-a) / (long double)(N-1) ;
+	long double result = 0.0;
+	result+= func(a);
+	long double point = a+h;
+	for (int i = 2; i <= N-2; ++i)
+	{
+		if(i%2 == 0) //se siamo al passo pari moltiplica per 4, altrimenti per 2
+		{
+			result+=4*func(point);
+		}
+		else
+		{
+			result+=2*func(point);
+		}
+		point+=h;
+	}
+	result+=func(b);
+	result*=(h/3);
+	return result;	
+}
