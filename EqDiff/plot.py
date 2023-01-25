b1a71b7bc9bfa259ec7075bcfb0c3b806daefd35
EdoMacbook <bertoliedoardo99@gmail.com>
2023-01-25 14:16:52 +0100

    Finire ex14

--- EqDiff/plot.py
+++ EqDiff/plot.py
@@ -12,23 +12,10 @@ B = np.genfromtxt("DataRK2.txt", delimiter=',',dtype=np.longdouble, usecols=1)
 C = np.genfromtxt("DataRK4.txt", delimiter=',',dtype=np.longdouble, usecols=0)
 D = np.genfromtxt("DataRK4.txt", delimiter=',',dtype=np.longdouble, usecols=1)
 
-h = np.array([0.9,0.5,0.1,0.05,0.01,0.005,0.001])
-E = np.array([0.90000000000000002220,1.3750000000,1.0743914914,1.0392067962,1.00788013351727952433,1.00393233478518400029,1.00078499006596076046]) 
-RK2 = np.array([0.90000000000000002220,1.0234375000,0.9978552766,0.9998211900,0.99999989967605931301,0.99999971265094572426,0.99999968333642241623])
-RK4 = np.array([0.77849999999999996996,0.9971297935,0.9974947947,0.9997837592,0.99999968293064017341,0.99999968293179403715,0.99999968293183459796])
-
-for i in range(len(h)):
-	h[i] = np.log(h[i])
-
-for i in range(len(h)):
-	E[i] = np.log(np.absolute(1 - E[i]))
-
-for i in range(len(h)):
-	RK2[i] = np.log(np.absolute(1 - RK2[i]))
-
-for i in range(len(h)):
-	RK4[i] = np.log(np.absolute(1 - RK4[i]))
-
+h = np.genfromtxt("h.txt", dtype=np.double, usecols=0)
+E = np.genfromtxt("devsE.txt", dtype=np.double, usecols=0)
+RK2 = np.genfromtxt("devsRK2.txt", dtype=np.double, usecols=0)
+RK4 = np.genfromtxt("devsRK4.txt", dtype=np.double, usecols=0)
 
 plt.style.use('_mpl-gallery')
 
@@ -39,7 +26,7 @@ plt.tight_layout() #fai in modo che il grafico si centri bene nella figura
 fig.set_size_inches(30/2.54, 30/2.54)
 
 
-lsp = np.linspace(np.amin(h),np.amax(h),1000)
+lsp = np.linspace(-8,2,1000)
 
 #fig.text(0.5,0.04,"x", ha='center')
 #fig.text(0.04,0.5, "f(x)", va='center',rotation='vertical')
@@ -60,16 +47,16 @@ RK4f = np.poly1d(fitRK4)
 ax.plot(lsp,RK4f(lsp), color="purple")
 
 
-ax.scatter(h,E, color = "green")
-ax.scatter(h,RK2, color="red")
-ax.scatter(h,RK4, color="purple")
+ax.scatter(h,E, color = "green", s=1)
+ax.scatter(h,RK2, color="red",s=1)
+ax.scatter(h,RK4, color="purple",s=1)
 #ax.plot(lsp,np.sin(lsp) - 10, color = "blue")
 #axs[1].plot(C,D, color = "purple")
 
 ax.set_xlabel("h, passi di integrazione")
 ax.set_ylabel("Deviazioni dalla soluzione esatta")
 ax.set_title("Deviazione dalla soluzione esatta a x = " + r'$\frac{\pi}{2}$')
-ax.legend(["Eulero","Runge Kutta 2","Runge Kutta 4"])
+ax.legend(["Eulero","Runge Kutta 2","Runge Kutta 4"], loc="upper left")
 #axs[1].legend(["Runge Kutta 4"])
 #axs[2].legend(["Soluzione esatta"])
 
