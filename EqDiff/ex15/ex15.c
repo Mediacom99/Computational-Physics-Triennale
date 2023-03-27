0ecd1f9c8c2d5cdb02e452f3cacca2ba6ce4edef
EdoMacbook <bertoliedoardo99@gmail.com>
2023-03-27 14:31:48 +0200

    Finito ex15, inizio ex16

--- EqDiff/ex15/ex15.c
+++ EqDiff/ex15/ex15.c
@@ -8,11 +8,28 @@
 // x segue la funzione soluzione (f(t)), y segue la derivata(df/dt), z la variabile indipendente (t)
 //fd è la funzione sull'eq differenziale della derivata
 //ff è la funzione sull'eq differenziale della soluzione
+
+/*
+Pendolo completo:
 long double fd(long double x, long double y, long double z)
 {
 	return -sin(x);
 }
+*/
 
+/*
+// Pendolo completo + attrito
+long double fd(long double x, long double y, long double z)
+{
+	return -sin(x) - 0.1*y;
+}
+*/
+
+// Pendolo completo + attrito +  forzante
+long double fd(long double x, long double y, long double z)
+{
+	return -sin(x) - 0.1*y + 0.1*sin((2.0/3.0)*z);
+}
 
 int main(int argc, char const *argv[])
 {	
@@ -20,37 +37,12 @@ int main(int argc, char const *argv[])
 	long double f0 = 0; //Dato iniziale soluzione
 	long double df0 = 1; //Dato iniziale derivata soluzione
 	long double T = 15.70796327*4;
-	long double h = 0.1;
+	long double h = 0.01;
 	long double n = T/h;
 	EuleroII(fd, f0, df0 , 0 , h ,n);
 	Runge_Kutta2(fd, f0 , df0 , 0 , h ,n);
 	Runge_Kutta4(fd,f0,df0,0,h,n);
 	
-	//Questa parte serve per il grafico della deviazione dell'esericizio 14
-	/*
-	FILE* Etxt = fopen("devsE.txt","w");
-
-	FILE* RK2txt = fopen("devsRK2.txt","w");
-
-	FILE* RK4txt = fopen("devsRK4.txt","w");
-
-	FILE* htxt = fopen("h.txt","w");
-
-	long double T = 1.570796327;
-	long double h = 0.01;
-	while(h <= 0.5)
-	{
-	fprintf(htxt, "%.20Lf\n", logl(fabsl(h)));
-	long double n = T/h;
-	EuleroII(fd, 0 , 1 , 0 , h ,n, Etxt);
-	Runge_Kutta2(fd, 0 , 1 , 0 , h ,n, RK2txt);
-	Runge_Kutta4(fd,0,1,0,h,n, RK4txt);
-	h+=0.01;
-	}
-	fclose(Etxt);
-	fclose(RK2txt);
-	fclose(RK4txt);
-	*/
 	return 0;
 
 }
